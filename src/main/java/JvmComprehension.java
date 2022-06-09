90587565ece110c49960eaab22e95f8869c40360
Nakat13 <100234772+Nakat13@users.noreply.github.com>
2022-06-09 21:43:01 +0300

    Update JvmComprehension.java

--- src/main/java/JvmComprehension.java
+++ src/main/java/JvmComprehension.java
@@ -1,10 +1,11 @@
 public class JvmComprehension {
+    // загрузка класса в метаспейс
     public static void main(String[] args) {
         int i = 1;                      // 1
         Object o = new Object();        // 2
         Integer ii = 2;                 // 3
-        printAll(o, i, ii);             //4
-        System.out.println("finished"); //7
+        printAll(o, i, ii);             // 4
+        System.out.println("finished"); // 7
     }
 
     private static void printAll(Object o, int i, Integer ii) {
